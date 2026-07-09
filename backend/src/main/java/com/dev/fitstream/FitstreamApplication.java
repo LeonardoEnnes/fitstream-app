15416a0d25895e94545f5772706bdbf237647b9d
Leonardo Ennes <leonardoennes7@gmail.com>
2026-07-09 11:53:25 -0300

    chore: add initial backend project structure

---
+++ backend/src/main/java/com/dev/fitstream/FitstreamApplication.java
@@ -0,0 +1,13 @@
+package com.dev.fitstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitstreamApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dev/fitstream/FitstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.fitstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
