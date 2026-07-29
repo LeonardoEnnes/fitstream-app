5b86442bd877609a551b2a30611a82cc1ceb0cde
Leonardo Ennes <leonardoennes7@gmail.com>
2026-07-29 16:27:00 -0300

    feat: enhance MealForm with Field components and validation improvements

--- frontend/src/components/layout/MealForm.tsx
+++ frontend/src/components/layout/MealForm.tsx
@@ -4,6 +4,11 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Input} from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
+import {
+  FieldLabel,
+} from "@/components/ui/field"
+
+0
 interface MealFormProps {
     onSuccess?: () => void;
 }
@@ -32,34 +37,94 @@ export function MealForm({ onSuccess } : MealFormProps) {
     <form onSubmit={handleSubmit(onSubmit)} className="space-y-4" >
 
         <div className="space-y-1.5">
-            <label className= "text-sm font-light leading-none">Nome da Refeição</label>
-            <Input placeholder="Ex: Almoço" {...register("name")} />
-            {errors.name && <span className="text-xs text-red-500">{errors.name.message}</span>}
+            <FieldLabel htmlFor="input-required" className="text-sm font-light">
+                Nome da Refeição <span className="text-destructive">*</span> {/*talvez eu remova essa bolinha*/}
+            </FieldLabel>
+
+            <Input 
+                placeholder="Ex: Almoço" 
+                aria-invalid={!!errors.name}
+                {...register("name")}
+            />
+            
+            {errors.name && 
+                <span className="text-xs text-red-500">
+                        {errors.name.message}
+                </span>
+            }
         </div>
 
         <div className="grid grid-cols-2 gap-3">
             <div className="space-y-1.5">
-                <label className="text-sm font-light">Calorias</label>
-                <Input placeholder="Ex: 150" {...register("calories", { valueAsNumber: true })} />
-                {errors.calories && <span className="text-xs text-red-500">{errors.calories.message}</span>}
+                <FieldLabel htmlFor="input-required" className="text-sm font-light">
+                    Calorias <span className="text-destructive">*</span>
+                </FieldLabel>
+
+                <Input 
+                    placeholder="Ex: 150" 
+                    aria-invalid={!!errors.calories}
+                    {...register("calories", { valueAsNumber: true })} 
+                />
+
+                {errors.calories && 
+                    <span className="text-xs text-red-500">
+                        {errors.calories.message}
+                    </span>}
             </div>
 
             <div className="space-y-1.5">
-                <label className="text-sm font-light">Proteínas</label>
-                <Input placeholder="20g" {...register("protein", { valueAsNumber: true })} />
-                {errors.protein && <span className="text-xs text-red-500">{errors.protein.message}</span>}
+                <FieldLabel htmlFor="input-required" className="text-sm font-light">
+                    Proteínas <span className="text-destructive">*</span>
+                </FieldLabel>
+                
+                <Input 
+                    placeholder="20g" 
+                    aria-invalid={!!errors.protein}
+                    {...register("protein", { valueAsNumber: true })} 
+                    required
+                />
+
+                {errors.protein && 
+                    <span className="text-xs text-red-500">
+                        {errors.protein.message}
+                    </span>
+                }
+
             </div>
 
             <div className="space-y-1.5">
-                <label className="text-sm font-light">Carboidratos</label>
-                <Input placeholder="90g" {...register("carbs", { valueAsNumber: true })} />
-                {errors.carbs && <span className="text-xs text-red-500">{errors.carbs.message}</span>}
+                <FieldLabel htmlFor="input-required" className="text-sm font-light">
+                    Carboidratos <span className="text-destructive">*</span>
+                </FieldLabel>
+                <Input 
+                    placeholder="90g" 
+                    aria-invalid={!!errors.carbs}
+                    {...register("carbs", { valueAsNumber: true })} 
+                />
+                
+                {errors.carbs && 
+                    <span className="text-xs text-red-500">
+                        {errors.carbs.message}
+                    </span>
+                }
             </div>
-            
+
             <div className="space-y-1.5">
-                <label className="text-sm font-light">Gorduras</label>
-                <Input placeholder="50g" {...register("fat", { valueAsNumber: true })} />
-                {errors.fat && <span className="text-xs text-red-500">{errors.fat.message}</span>}
+                <FieldLabel htmlFor="input-required" className="text-sm font-light">
+                    Gorduras <span className="text-destructive">*</span>
+                </FieldLabel>
+
+                <Input 
+                    placeholder="50g" 
+                    aria-invalid={!!errors.fat}
+                    {...register("fat", { valueAsNumber: true })} 
+                />
+
+                {errors.fat && 
+                    <span className="text-xs text-red-500">
+                        {errors.fat.message}
+                    </span>
+                }
             </div>
         </div>
 
