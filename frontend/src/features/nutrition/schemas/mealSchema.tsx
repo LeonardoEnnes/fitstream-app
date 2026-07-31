f7374013475c59b2e32bb1d607bbc59697de03e4
Leonardo Ennes <leonardoennes7@gmail.com>
2026-07-31 14:07:48 -0300

    feat: enhance mealSchema validation and transformation for meal name

--- frontend/src/features/nutrition/schemas/mealSchema.tsx
+++ frontend/src/features/nutrition/schemas/mealSchema.tsx
@@ -3,9 +3,17 @@ import { z } from "zod";
 export const mealSchema = 
   z.object({
     name: z.string()
-    .max(60)
+    .trim() // tirando espaços em branco no inicio e no final da string
     .min(1, { message: "O nome da refeição é obrigatório"})
-    .nonempty({ message: "O nome da refeição não pode ser vazio" }), // será que faz sentido nesse contexto? 
+    .max(60)
+    .regex(
+      /^[a-zA-ZÀ-ÿ\s]+$/, // aceitar apenas letras e espaços
+      { message: "O nome da refeição só pode conter letras e espaços" }
+    ).transform(value => 
+      value.toLowerCase()
+      .replace(/\b\w/g, (match) => match.toUpperCase())  
+    )
+    ,
 
     calories: z.number({
         error: "O número de calorias é obrigatório"
@@ -22,6 +30,17 @@ export const mealSchema =
     fat: z.number({
       error: "O numero de gorduras é obrigatorio"
     }).min(0, { message: "O numero de gorduras não pode ser negativo"}),
-  })
+  }).refine( // serve para validar que o campo carbs seja maior que zero, se ele n for vai retornar um erro
+      (data) => 
+        data.calories > 0 || 
+        data.protein > 0 || 
+        data.carbs > 0 || 
+        data.fat > 0,
 
-export type MealFormData = z.output<typeof mealSchema>; // isso serve para tipar o formState do react-hook-form, garantindo que os tipos de dados do formulário estejam corretos e consistentes com o esquema definido.
+      {
+        message: "Informe pelo menos um valor nutricional maior que zero",
+        path: ["calories"]
+      }
+   )
+
+export type MealFormData = z.output<typeof mealSchema>; 
