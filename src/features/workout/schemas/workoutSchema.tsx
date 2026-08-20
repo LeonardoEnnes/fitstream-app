3a60ed26a14b2362a8fb5ad30d523fe7d651e71b
Leonardo Ennes <leonardoennes7@gmail.com>
2026-08-20 16:33:26 -0300

    chore: reorganize folder project structure

--- src/features/workout/schemas/workoutSchema.tsx
+++ src/features/workout/schemas/workoutSchema.tsx
@@ -6,6 +6,7 @@ export const workoutSchema = z.object({
     sets: z.coerce.number().min(1, { message: "Mínimo de 1 série" }),
     reps: z.coerce.number().min(1, { message: "Mínimo de 1 repetição" }),
     weight: z.coerce.number().min(0, { message: "A carga não pode ser negativa" }),
+    /// colocar uma dATA ASQ
 }
 )
 
